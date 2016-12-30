0fd1a30620af05606ab66adf297f892851149c59
Benjamin Pang <molarmanful@users.noreply.github.com>
2016-12-29 17:51:53 -0800

    Update date.js

--- date.js
+++ date.js
@@ -1,1 +1,16 @@
-cm.date=x=>{}
+cm.date=x=>(
+  D=new Date(),
+  obj({
+    str:str(D+''),
+    d:num(D.getDate()),
+    D:num(D.getDay()),
+    y:num(D.getFullYear()),
+    h:num(D.getHours()),
+    ms:num(D.getMilliseconds()),
+    m:num(D.getMinutes()),
+    M:num(D.getMonth()),
+    s:num(D.getSeconds()),
+    t:num(D.getTime()),
+    off:num(D.getTimezoneOffset())
+  })
+)
